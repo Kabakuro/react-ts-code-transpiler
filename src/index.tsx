61c7e54e1b41a9cbe1e5e9510f7e664804289a61
Henry Lyons <ibooshyou@gmail.com>
2021-05-06 09:55:26 +0100

    Added iframe to output the transpiled code

--- src/index.tsx
+++ src/index.tsx
@@ -9,22 +9,23 @@ const App: React.FC = () => {
     const [code, setCode] = useState('');
 
     const ref = useRef<any>();
+    const iframe = useRef<any>();
 
     const startService = async () => {
         ref.current = await esbuild.startService({
             worker: true,
-            wasmURL: '/esbuild.wasm',
-          })
-    }
+            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+          });
+    };
 
     useEffect(() => {
         startService();
-    }, [])
+    }, []);
 
     const onClick = async () => {
         if (!ref.current) {
             return;
-        }
+        };
 
         const result = await ref.current.build({
             entryPoints: ['index.js'],
@@ -35,12 +36,25 @@ const App: React.FC = () => {
                 'process.env.NODE_ENV': '"production"',
                 global: 'window'
             }
-        })
+        });
 
-        // console.log(result)
+        // setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    };
 
-        setCode(result.outputFiles[0].text);
-    }
+    const html = `
+      <html>
+        <head></head>
+        <body>
+            <div id="root"></div>
+            <script>
+                window.addEventListener('message', (event) => {
+                    eval(event.data);
+                }, false);
+            </script>
+        </body>
+      </html>
+    `;
 
     return (
        <div>
@@ -55,11 +69,12 @@ const App: React.FC = () => {
                </button>
            </div>
            <pre>{ code }</pre>
+           <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html}/>
        </div>
-    )
-}
+    );
+};
 
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
-)
+);
