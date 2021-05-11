042ebf1e6ac6133baf93cbeb57f2c1afb5d778ec
Henry Lyons <ibooshyou@gmail.com>
2021-05-11 13:50:50 +0100

    added basic css to the text editor and some clean up

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
 import { useState, useEffect, useRef } from 'react';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
 
 const App: React.FC = () => {
     const [input, setInput] = useState('');
-
+    const [code, setCode] = useState('');
     const ref = useRef<any>();
-    const iframe = useRef<any>();
 
     const startService = async () => {
         ref.current = await esbuild.startService({
@@ -27,8 +28,6 @@ const App: React.FC = () => {
             return;
         };
 
-        iframe.current.srcdoc = html;
-
         const result = await ref.current.build({
             entryPoints: ['index.js'],
             bundle: true,
@@ -40,29 +39,8 @@ const App: React.FC = () => {
             }
         });
 
-        // setCode(result.outputFiles[0].text);
-        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+        setCode(result.outputFiles[0].text);
     };
-
-    const html = `
-      <html>
-        <head></head>
-        <body>
-            <div id="root"></div>
-            <script>
-                window.addEventListener('message', (event) => {
-                    try {
-                        eval(event.data);
-                    } catch (err) {
-                        const root = document.querySelector('#root');
-                        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-                        console.error(err);
-                    }
-                }, false);
-            </script>
-        </body>
-      </html>
-    `;
 
     return (
        <div>
@@ -80,7 +58,7 @@ const App: React.FC = () => {
                    Submit
                </button>
            </div>
-           <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html}/>
+           <Preview code={code}/>
        </div>
     );
 };
