4758cf0f0311b43c6a3584a2c336992d78f6ed3b
Henry Lyons <ibooshyou@gmail.com>
2021-05-11 14:08:53 +0100

    more code clean up and refactoring

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import CodeEditor from './code-editor';
+import Preview from './preview';
+import bundle from '../bundler';
+
+const CodeCell: React.FC = () => {
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+
+    const onClick = async () => {
+        const output = await bundle(input);
+        setCode(output);
+    };
+
+    return (
+       <div>
+           <CodeEditor 
+             initialValue="const a = 1;"
+             onChange={(value) => setInput(value)}
+           />
+           <textarea 
+                onChange={e => setInput(e.target.value)}
+                value={input}
+            >
+           </textarea>
+           <div>
+               <button onClick={onClick}>
+                   Submit
+               </button>
+           </div>
+           <Preview code={code}/>
+       </div>
+    );
+};
+
+export default CodeCell;
