57d191876683463927b84519553fc5f9f58cbe47
Henry Lyons <ibooshyou@gmail.com>
2021-05-13 15:36:25 +0100

    added additional css

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import bundle from '../bundler';
+import Resizable from './resizable';
 
 const CodeCell: React.FC = () => {
     const [input, setInput] = useState('');
+    const [err, setErr] = useState('');
     const [code, setCode] = useState('');
 
-    const onClick = async () => {
-        const output = await bundle(input);
-        setCode(output);
-    };
+    useEffect(() => {
+        const timer = setTimeout(async () => {
+            const output = await bundle(input);
+            setCode(output.code);
+            setErr(output.err);
+        }, 750);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [input])
 
     return (
-       <div>
-           <CodeEditor 
-             initialValue="const a = 1;"
-             onChange={(value) => setInput(value)}
-           />
-           <textarea 
-                onChange={e => setInput(e.target.value)}
-                value={input}
-            >
-           </textarea>
-           <div>
-               <button onClick={onClick}>
-                   Submit
-               </button>
-           </div>
-           <Preview code={code}/>
-       </div>
+        <Resizable direction="vertical">
+            <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
+                <Resizable direction="horizontal">
+                    <CodeEditor 
+                        initialValue="const a = 1;"
+                        onChange={(value) => setInput(value)}
+                    />
+                </Resizable>
+                <Preview code={code} err={err} />
+            </div>
+        </Resizable>
     );
 };
 
